62019558c2ac5886ba843d8f377be364e0296e41
isadseta@gmail.com <isadseta@hotmail.com>
2025-11-23 14:24:50 +0100

    - miss loger name definition

--- operators/cmd/examagent/main.go
+++ operators/cmd/examagent/main.go
@@ -30,7 +30,7 @@ import (
 
 func main() {
 	examagent.Options.Init()
-	log := textlogger.NewLogger(textlogger.NewConfig()).WithName("list")
+	log := textlogger.NewLogger(textlogger.NewConfig()).WithName("examagent")
 
 	if err := examagent.Options.Parse(); err != nil {
 		log.Error(err, "invalid configuration")
